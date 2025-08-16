195317a289809e07a2b4339aaef4fa3e695733a5
ritik-agrawal <ritikagrawal1697@gmail.com>
2025-08-16 15:02:52 +0530

    changes: - presigned url validity - document entity changes - mapped document to user profile
    Ticket:
    https://github.com/bannrx/campaign-service/issues/7

--- src/main/java/com/bannrx/common/service/UserProfileService.java
+++ src/main/java/com/bannrx/common/service/UserProfileService.java
@@ -4,6 +4,7 @@ import com.bannrx.common.persistence.entities.UserProfile;
 import com.bannrx.common.repository.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import rklab.utility.expectations.InvalidInputException;
 
 
 @Service
@@ -15,4 +16,18 @@ public class UserProfileService {
         userProfileRepository.delete(profile);
         return "user profile deleted successfully";
     }
+
+    /**
+     * Fetches the user profile using the userId
+     *
+     * @param userId user_id
+     * @return userProfile
+     * @throws InvalidInputException in case userProfile not found
+     */
+    public UserProfile fetchByUserId(final String userId) throws InvalidInputException {
+        return userProfileRepository.findByUserId(userId)
+                .orElseThrow(() ->
+                        new InvalidInputException("User Profile not found with user id "+userId));
+    }
+
 }
